a1fd2980a4a53bd59ba734bd57125d73c9ca20e3
Vinicius Nogueira Ribeiro Garcia <viniciusnrgarcia@gmail.com>
2024-03-09 00:37:15 -0300

    Docker-compose

--- run.sh
+++ run.sh
@@ -1,4 +1,18 @@
-docker compose down \
-&& docker compose rm -f \
-&& docker compose up --force-recreate -d \
-&& docker compose logs -f
+#!/bin/bash
+
+echo docker compose down
+docker compose down
+docker compose rm -f
+
+echo Clean Environment
+containers=$(docker ps -a -q)
+
+if [ -n "$containers" ]; then
+    docker rm -f $containers
+fi
+
+docker volume prune --all --force
+
+docker compose up --force-recreate -d
+
+docker compose logs -f
